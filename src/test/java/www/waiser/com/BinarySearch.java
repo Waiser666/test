032c97acc7a12bc32e2a9bb28f4405c63cb6f8d8
mengwei <569826969@qq.com>
2021-03-04 10:44:42 +0800

    二分查找

---
+++ src/test/java/www/waiser/com/BinarySearch.java
@@ -0,0 +1,28 @@
+package www.waiser.com;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @Author mengwei
+ * @Date 2021/3/4
+ */
+public class BinarySearch {
+    int binarySearch(int[] nums, int target){
+        if(nums == null || nums.length == 0)
+            return -1;
+
+        int left = 0, right = nums.length - 1;
+        while(left <= right){
+            // Prevent (left + right) overflow
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target){ return mid; }
+            else if(nums[mid] < target) { left = mid + 1; }
+            else { right = mid - 1; }
+        }
+
+        // End Condition: left > right
+        return -1;
+    }
+}
